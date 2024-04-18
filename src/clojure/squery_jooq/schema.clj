3300d2c268fa6f9705ebdffe6219c8d227cb4541
tkaryadis <tkaryadis@gmail.com>
2024-04-18 23:35:53 +0300

    admin-commands

--- src/clojure/squery_jooq/schema.clj
+++ src/clojure/squery_jooq/schema.clj
@@ -1,20 +1,21 @@
 (ns squery-jooq.schema
   (:import (org.jooq DataType)
-           (java.sql Date Timestamp)))
+           (java.sql Date Timestamp)
+           (org.jooq.impl SQLDataType)))
 
 (def schema-types
   {
    ;:binary      binary
-   :boolean   Boolean
-   :byte      Byte
-   :date      Date
-   :double    Double
-   :float     Float
-   :integer   Integer
-   :int       Integer
-   :long      Long        ;;bigint
-   ;:null      DataTypes/NullType
-   :short     Short
-   :string    String        ;;varchar
-   :timestamp Timestamp
+   :boolean   SQLDataType/BOOLEAN
+   :byte      SQLDataType/TINYINT
+   :date      SQLDataType/DATE
+   :double    SQLDataType/DOUBLE
+   :float     SQLDataType/FLOAT
+   :integer   SQLDataType/INTEGER
+   :int       SQLDataType/INTEGER
+   :long      SQLDataType/BIGINT
+   ;:nil
+   :short     SQLDataType/SMALLINT
+   :string    SQLDataType/VARCHAR
+   :timestamp SQLDataType/TIMESTAMP
    })
--- test/squery_jooq/c4_6ddl/t3create.clj
+++ test/squery_jooq/c4_6ddl/t3create.clj
@@ -5,6 +5,7 @@
             [squery-jooq.commands.update :refer :all]
             [squery-jooq.commands.admin :refer :all]
             [squery-jooq.state :refer [connect ctx]]
+            [squery-jooq.schema :refer [schema-types]]
             [squery-jooq.printing :refer [print-results print-sql ]]
             [clojure.core :as c])
   (:refer-clojure)
@@ -12,8 +13,8 @@
     (java.sql Timestamp)
     (java.time Instant)
     (java.util Date)
-    (org.jooq SQLDialect DSLContext Field Select Table SelectFieldOrAsterisk)
-    (org.jooq.impl DSL QOM$Lateral SelectImpl)
+    (org.jooq DataType SQLDialect DSLContext Field Select Table SelectFieldOrAsterisk)
+    (org.jooq.impl DSL QOM$Lateral SQLDataType SelectImpl)
     (org.jooq.conf Settings StatementType)))
 
 ;(connect "mysql")
@@ -34,4 +35,63 @@
 
 ;;TODO 4.6.3.7. CREATE SEQUENCE
 
+(defn get-column-type [col]
+  (let [col-type (if (keyword? col)
+                   (get schema-types :string)
+                   (get schema-types (second col) (second col)))
+        options (get col)]
+    ))
 
+
+(defn get-column-name [col]
+  (if (keyword? col)
+    (name col)
+    (first col)))
+
+
+
+(defn add-table-columns [table-obj cols]
+  (loop [cols cols]
+    (if (empty? cols)
+      table-obj
+      (let [cur-col (first cols)
+            cur-col-name (get-column-name cur-col)
+            cur-col-type (get-column-type cur-col)
+            nillable (nillable-column cur-col)]
+        (recur (do
+                 (cond
+                   (= nillable 0)
+                   (.column table-obj cur-col-name cur-col-type)
+
+                   (= nillable 1)
+                   (.column table-obj cur-col-name (.null_ cur-col-type))
+
+                   (= nillable -1)
+                   (.column table-obj cur-col-name (.notNull cur-col-type)))
+                 (rest cols)))))))
+
+(defn create-table [table-name cols]
+  (-> ^DSLContext @ctx
+      (.createTable (name table-name))
+      (add-table-columns cols)))
+
+;;(def my-manual-schema
+;  (build-schema
+;    [:DEST_COUNTRY_NAME
+;     :ORIGIN_COUNTRY_NAME
+;     [:count :long false (Metadata/fromJson "{\"hello\":\"world\"}")]]))
+
+;;candidate keys i delcare them as unique
+(create-table :mytable
+              [:col
+               ;;3rd arg is for nil?
+               [:col :type/orValue :true-false-missing]]
+              {:constraints [:constraint-arg1
+                             :constraint-arg2
+                             ;;primaryKey("column1")
+                             ;;constraint("pk").primaryKey("column1")
+                             ;;unique("column1"),
+                             ;;unique("column2", "column3")
+                             ;;constraint("fk").foreignKey("column1").references("other_table", "other_column1")
+                             ;;check(field(name("column1"), INTEGER).gt(0))
+                             ]})
